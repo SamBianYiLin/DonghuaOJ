1b42389cdeabab7947cd814ec9098d1a27c2b202
samiliant <sam614@163.com>
2026-03-14 21:01:33 +0800

    2026/03/14 BFS  DFS and string related question

--- Question_119_ShoppingInMall/ShoppingInMall.cpp
+++ Question_119_ShoppingInMall/ShoppingInMall.cpp
@@ -4,13 +4,40 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+struct ProductPart
+{
+	int no;
+	int quantity;
+};
+struct Products
+{
+	vector<ProductPart> eachProduct;
+	int totalPrice;
+};
 int main() {
 	int catagories;
-	while (scanf("%d", &catagories) > 0) {
+	vector<Products> discounts;
+	//	读取优惠类别与优惠内容
 		for (int x = 0; x < catagories; x++){
-
+		int n;
+		scanf("%d", &n);
+		Products products;
+		//	一个优惠分类中有几组商品
+		for (int y = 0; y < n; y++){
+			int no, quantity;
+			scanf("%d %d", &no, &quantity);
+			products.eachProduct.push_back({ no,quantity });
 		}
+		//	读入总价
+		int total; 
+		scanf("%d", &total);
+		products.totalPrice = total;
+		discounts.push_back(products);
 	}
+	int productKinds;
+	scanf("%d", &productKinds);
+	for (int x = 0; x < productKinds; x++){
 
+	}
 	return 0;
 }
